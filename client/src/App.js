f181a95755b1923a10cc4306cb191e0bc5ad17dc
Kurt LaVacque <lavacquek@icloud.com>
2021-02-01 17:08:28 -0600

    Styled the Gcode Continous Printing App

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,10 @@ function App() {
 	const [ final_gcode, set_final_gcode ] = useState('');
 	const [ directory, set_directory ] = useState('');
 	const [ filename, set_filename ] = useState('');
+	const [ gcode_1_name, set_gcode_1_name ] = useState('');
+	const [ gcode_2_name, set_gcode_2_name ] = useState('');
+	const [ status, set_status ] = useState('');
+	const [ loading, set_loading ] = useState(false);
 
 	const remove_print = `G1 X105 Y195 Z50 F8000 ; Move up and back
   
@@ -39,21 +43,17 @@ function App() {
 		reader.onload = async (e) => {
 			text = e.target.result;
 			const gcode = text.split('\n');
-			// console.log(e.target.result);
 			let beginning_1_array = [];
 			let beginning_1_boolean = true;
 			let middle_start = 0;
 			let middle_finished = 0;
 			for (let i = 0; i <= 100; i++) {
-				// console.log(gcode[i]);
 				if (gcode[i] === 'M83 ; use relative distances for extrusion') {
 					beginning_1_boolean = false;
 					beginning_1_array = [ ...beginning_1_array, gcode[i] ];
 					middle_start = i + 1;
-					console.log({ middle_start });
 				} else if (beginning_1_boolean) {
 					beginning_1_array = [ ...beginning_1_array, gcode[i] ];
-					// return;
 				}
 			}
 
@@ -68,21 +68,18 @@ function App() {
 					console.log({ middle_finished });
 				} else if (ending_1_boolean) {
 					ending_1_array = [ ...ending_1_array, gcode[i] ];
-					// return;
 				}
 			}
 			const middle_1_array = gcode.slice(middle_start, middle_finished);
-			// console.log({ beginning_1_array, middle_1_array, ending_1_array: ending_1_array });
 			set_beginning_1(beginning_1_array);
 			set_middle_1(middle_1_array);
 			set_ending_1(ending_1_array);
-
-			// create_new_gcode(beginning_1, middle_1, ending_1_array);
 		};
 
 		reader.readAsText(e.target.files[0]);
 		set_filename(document.getElementById('file_1').files[0].name);
 		set_file(document.getElementById('file_1').files[0]);
+		set_gcode_1_name(document.getElementById('file_1').files[0].name);
 	};
 	const showFile_2 = async (e) => {
 		e.preventDefault();
@@ -92,21 +89,17 @@ function App() {
 		reader.onload = async (e) => {
 			text = e.target.result;
 			const gcode = text.split('\n');
-			// console.log(e.target.result);
 			let beginning_2_array = [];
 			let beginning_2_boolean = true;
 			let middle_start = 0;
 			let middle_finished = 0;
 			for (let i = 0; i <= 100; i++) {
-				// console.log(gcode[i]);
 				if (gcode[i] === 'M83 ; use relative distances for extrusion') {
 					beginning_2_boolean = false;
 					beginning_2_array = [ ...beginning_2_array, gcode[i] ];
 					middle_start = i + 1;
-					console.log({ middle_start });
 				} else if (beginning_2_boolean) {
 					beginning_2_array = [ ...beginning_2_array, gcode[i] ];
-					// return;
 				}
 			}
 
@@ -118,29 +111,25 @@ function App() {
 					ending_2_array = [ ...ending_2_array, 'G4 ; wait' ];
 					ending_2_boolean = false;
 					middle_finished = i;
-					console.log({ middle_finished });
 				} else if (ending_2_boolean) {
 					ending_2_array = [ ...ending_2_array, gcode[i] ];
-					// return;
 				}
 			}
 			const middle_2_array = gcode.slice(middle_start, middle_finished);
-			// console.log({ beginning_2_array, middle_2_array, ending_2_array });
 			const ending_2_forward = [ ...ending_2_array ];
 			const ending_2_backwards = [ ...ending_2_array ].reverse();
-			// ending_2_backwards.reverse();
 			console.log({ ending_2_forward, ending_2_backwards });
 			set_beginning_2(beginning_2_array);
 			set_middle_2(middle_2_array);
 			set_ending_2(ending_2_backwards);
-
-			// create_new_gcode(beginning_2, middle_2, ending_2);
 		};
 
 		reader.readAsText(e.target.files[0]);
+		set_gcode_2_name(document.getElementById('file_2').files[0].name);
 	};
 
 	const create_new_gcode = async () => {
+		set_loading(true);
 		let gcode_array = [ beginning_1 ];
 		console.log(number_of_copies);
 		if (number_of_copies === 2) {
@@ -167,7 +156,8 @@ function App() {
 		console.log({ gcode });
 		const response = await API.export_gcode(update_filename(filename), gcode);
 		console.log({ response });
-		// set_final_gcode(gcode);
+		set_loading(false);
+		set_status(`Created ${update_filename(filename)}`);
 	};
 
 	const update_filename = (filename) => {
@@ -177,36 +167,79 @@ function App() {
 		return new_filename;
 	};
 
-	// function readmultifiles(e) {
-	//  const files = e.currentTarget.files;
-	//  const num_files = Object.keys(files).length;
-	//  console.log({ num_files });
-	//  Object.keys(files).forEach((i) => {
-	//    const file = files[i];
-	//    const reader = new FileReader();
-	//    reader.onload = (e) => {
-	//      //server call for uploading or reading the files one-by-one
-	//      //by using 'reader.result' or 'file'
-	//      console.log({ file: e.target.result });
-	//    };
-	//    reader.readAsBinaryString(file);
-	//  });
-	// }
-
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				{/* <input type="text" defaultValue={filename} onChange={(e) => set_filename(e.target.value)} /> */}
-				<input type="file" id="file_1" multiple onChange={(e) => showFile_1(e)} />
-				<input type="file" multiple onChange={(e) => showFile_2(e)} />
-				<input
-					type="number"
-					defaultValue={number_of_copies}
-					onChange={(e) => set_number_of_copies(e.target.value)}
-				/>
-				<button onClick={() => create_new_gcode()}>Make Continuous Gcode</button>
+		<div className="m-auto">
+			<header className="p-1rem mb-2rem jc-b">
+				<div className="logo h-125px w-125px">
+					<img className="zoom logo_s" src="/glow_logo_optimized.png" alt="Glow LEDs Logo" title="Big Logo" />
+				</div>
+				<h1 className="ta-c fs-50px ">Gcode Continous Printing</h1>
+				<div className="w-125px" />
 			</header>
+			<div>
+				{loading && (
+					<div className="jc-c column">
+						<img
+							src={process.env.PUBLIC_URL + '/loading.gif'}
+							className="loading_gif"
+							alt="Loading Circle"
+							title="Loading Circle"
+						/>
+						<img
+							src={process.env.PUBLIC_URL + '/loading_overlay.png'}
+							className="loading_png"
+							alt="Loading Overlay"
+							title="Loading Overlay"
+						/>
+						{/* {loading_message()} */}
+					</div>
+				)}
+			</div>
+			<div className="column m-auto max-w-500px w-100per form">
+				<div className="form-item">
+					<label className="mr-1rem w-50per fw-800">1/2 gcode</label>
+					<label className="btn primary w-50per">
+						<label className="">Choose gcode file</label>
+						<input
+							className="btn primary"
+							type="file"
+							id="file_1"
+							multiple
+							onChange={(e) => showFile_1(e)}
+						/>
+					</label>
+				</div>
+				<label className="form-item">{gcode_1_name}</label>
+				<div className="form-item">
+					<label className="mr-1rem w-50per fw-800">2/2 gcode</label>
+					<label className="btn primary w-50per">
+						<label className="">Choose gcode file</label>
+						<input
+							className="btn primary"
+							type="file"
+							id="file_2"
+							multiple
+							onChange={(e) => showFile_2(e)}
+						/>
+					</label>
+				</div>
+				<label className="form-item">{gcode_2_name}</label>
+				<div className="form-item">
+					<label className="mr-1rem w-50per fw-800">Number of Copies</label>
+					<input
+						type="number"
+						className="w-50per"
+						defaultValue={number_of_copies}
+						onChange={(e) => set_number_of_copies(e.target.value)}
+					/>
+				</div>
+				<div className="form-item">
+					<button className="btn primary w-100per" onClick={() => create_new_gcode()}>
+						Make Continuous Gcode
+					</button>
+				</div>
+				{status && <label className="form-item btn secondary">{status}</label>}
+			</div>
 		</div>
 	);
 }
